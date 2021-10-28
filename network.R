91f729e49db4e3a42167b360f033772fb6ed0535
Simo <75310327+simo-91@users.noreply.github.com>
2021-10-28 17:06:28 +0100

    Add files via upload
    added calcium levels + RFP-positive color point labels on graph

--- network.R
+++ network.R
@@ -34,16 +34,17 @@ HRAS5dpf_hind2_15min.graph.plt <- ggraph(HRAS5dpf_hind2_15min.graph,
                                                      option = "inferno",
                                                      direction = 1
                             )+
-                         # RFP positives
-                            # geom_node_point(aes(size = as.factor(HRAS5dpf_hind2_15min.posXY$RFP),
-                            #                     shape = as.factor(HRAS5dpf_hind2_15min.posXY$RFP)))+
-                            # scale_shape_manual(values = g.shapes.RFP, name = "RFP")+
-                            # scale_size_manual(values = g.sizes.RFP, name = "RFP")+
                           # Calcium levels
-                            geom_node_point(aes(fill = HRAS5dpf_hind2_15min.posXY$Mean.dF), 
-                                            shape = 21, size = 2)+
+                            geom_node_point(aes(fill = HRAS5dpf_hind2_15min.posXY$Mean.dF,
+                                                size = as.factor(HRAS5dpf_hind2_15min.posXY$RFP),
+                                                shape = as.factor(HRAS5dpf_hind2_15min.posXY$RFP),
+                                                colour = as.factor(HRAS5dpf_hind2_15min.posXY$RFP)))+
                             scale_fill_continuous(type = "viridis")+
-                            # scale_colour_manual(values = c("black", "#2ca25f"))+
+                            scale_size_manual(values = c("TRUE" = 3.5, "FALSE" = 2.5))+
+                            scale_shape_manual(values = c("TRUE" = 25, "FALSE" = 21))+
+                            scale_colour_manual(values = c("TRUE" = "#fc9272", "FALSE" = "black"))+
+                          # RFP positives
+
                             # geom_node_point(aes(fill = as.factor(HRAS5dpf_hind2_15min.posXY$synchron),
                             #                     size = as.factor(HRAS5dpf_hind2_15min.posXY$synchron),
                             #                     shape = as.factor(HRAS5dpf_hind2_15min.posXY$synchron)))+
