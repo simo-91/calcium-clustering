5d2b1ea5761f1ee2f26584c35ac36086838d30cd
Simo <Simo@SiegerLabs-MacBook-Pro.local>
2021-12-29 13:15:30 +0000

    Restructured and cleaned up the script a bit

--- calciumSuite2p.R
+++ calciumSuite2p.R
@@ -17,6 +17,7 @@ library(RcppCNPy)
 library(cowplot)
 library(ggpubr)
 library(mmand)
+library(rstudioapi)
 # Load raw Suite2p npy arrays using reticulate
 library(reticulate)
 np <-import("numpy")
@@ -94,6 +95,9 @@ spksSUM2$Mean <- spksSUM2$spksSUM2/nrow(spksSUM2)
 saveRDS(spksSUM2, file = "ID0025_spksSUM2.rds")
 write.csv(spksSUM2, file = "ID0025_spksSUM2.csv")
 
+# Average activity PER CELL (deconvolved peaks) ---------------------------
+ID0025.posXY$Mean <- rowMeans(spks)
+
 
 # # Raster+dendro all cells/time ggplot ------------------------------------------
 dfpeaks <- as.data.frame(t(spks))  # Doing this coercion will apply +1 to all cells numbers
@@ -125,10 +129,7 @@ meltPeaks$cell <- factor(x = meltPeaks$cell,
                          levels = peaks.rows$peaks.rows[peaks.order], 
                          ordered = TRUE)
 
-# Ggplot rasterwith dendro order
-# RFPcells <- unique(c(150, 48, 398, 118, 223, 20, 3, 224, 24, 130, 199, 320,184,176,97, 13, 2, 735, 1, 216, 74, 16, 79, 266, 85, 6, 200, 576, 257, 21, 56, 18, 502, 204, 64, 93, 31, 0, 399, 122))
-# meltPeaks$RFP <- meltPeaks$cell %in% RFPcells #highlight RFP cells
-
+# Ggplot raster with dendro order
 ID0025.raster.hc <- ggplot(meltPeaks, aes(time, cell))+
   geom_raster(aes(fill = `Ca2+`))+
   # geom_line(aes(color = RFP), alpha = .2)+
@@ -166,18 +167,31 @@ RFPsum$Perc <- RFPsum$`colSums(RFPt)`/nrow(RFPt)*100
 saveRDS(RFPsum, file = "ID0025_RFPsum.rds")
 write.csv(RFPsum, file = "ID0025_RFPsum.csv")
 
-# # Sum of all activity over time RFP+
+# Plot
+ID0025.RFPsum.plt <- ggplot(RFPsum, aes(Time, Perc))+
+  geom_line()+
+  theme_pubr()
+
+
+
+# Sum of all activity over time RFP+ --------------------------------------
 RFP <- subset(spks, rownames(spks) %in% ID0025.RFPcells) #select only RFP cells
-RFPsum2 <- as.data.frame(colSums(RFP)/nrow(RFP))
+RFPsum2 <- as.data.frame(colSums(RFP)/nrow(RFP)) #normalized by number of RFP cells
 RFPsum2$Time <- 0:(nrow(RFPsum2)-1)
 saveRDS(RFPsum2, file = "ID0025_RFPsum2.rds")
 write.csv(RFPsum2, file = "ID0025_RFPsum2.csv")
 
+ID0025.RFPsum2.plt <- ggplot(RFPsum2, aes(Time, `colSums(RFP)/nrow(RFP)`))+
+  geom_line()+
+  theme_pubr()+
+  geom_smooth()+
+  ylab("normalized dF/F")+
+  ylim(0, NA)
+ID0025.RFPsum2.plt.ylim <- layer_scales(ID0025.RFPsum2.plt)$y$get_limits()
 
 
-# RFPcells for activity/raster/dendrogram. Take care of using already normalized spks array
-# RFPnothresh <- subset(spks, rownames(spks) %in% RFPcells) #select only RFP cells
-
+# RFPcells for activity/hc/raster/dendrogram.  -------------------------------
+## Take care of using already normalized spks array
 # Raster ggplot
 dfpeaks.RFP <- as.data.frame(t(RFP))
 # colnames(dfpeaks.RFP) <- 1:ncol(dfpeaks.RFP)
@@ -188,7 +202,6 @@ colnames(meltPeaks.RFP) <- c('time','cell','Ca2+')
 # Hierarchical clustering
 hc.RFP <- hclust(dist(RFP, method = "euclidean"), method = "ward.D2")
 dhc.RFP <- as.dendrogram(hc.RFP)
-
 
 # Dendrogram
 RFP.dendro <- ggdendrogram(dhc.RFP, rotate = TRUE, labels = FALSE)+
@@ -209,7 +222,7 @@ meltPeaks.RFP$cell <- factor(x = meltPeaks.RFP$cell,
                          ordered = TRUE)
 
 # Ggplot rasterwith dendro order
-RFP.raster <- ggplot(meltPeaks.RFP, aes(time, cell))+
+ID0025.RFP.raster <- ggplot(meltPeaks.RFP, aes(time, cell))+
   geom_raster(aes(fill = `Ca2+`))+
   scale_fill_gradientn(colours=c("white", "grey20", "grey10", "black"))+
   theme_pubr()+
@@ -223,32 +236,43 @@ RFP.raster <- ggplot(meltPeaks.RFP, aes(time, cell))+
   ggtitle("ID0025 RFP+")
 
 # GRID raster/sums
-plots <- align_plots(RFP.raster, ID0025_RFPsum.plt, align = 'v', axis = 'l')
-ID0025.RFP.grid <- plot_grid(plots[[1]], ID0025_RFPsum.plt, ncol = 1, rel_heights = c(4.5,1))
+plots <- align_plots(RFP.raster, ID0025.RFPsum.plt, align = 'v', axis = 'l')
+ID0025.RFP.grid <- plot_grid(plots[[1]], ID0025.RFPsum.plt, ncol = 1, rel_heights = c(4.5,1))
 
-########################################################################################
 ######################################### dF/F #########################################
-########################################################################################
 
-# Average calcium levels over time. Here by using dF traces after some coding in python suite2p-environment
+# Average calcium levels over time ----------------------------------------
+## Here by using dF traces after executing "dF.py" to extract them
 dF <- as.data.frame(np$load("dF.npy", allow_pickle = TRUE)) #delta F calcium levels of all cells
 dF <- dF[,50:ncol(dF)] #select window
 # dF <- t(apply(dF, 1, function(x) (x - min(x))/(max(x)-min(x)))) # Normalize?
 dFPOS <- dF[positivesPLUSone,]
 rownames(dFPOS) <- positives
 
-# Average activity per cell (dF peaks)
+
+# Average activity per RFP+ cell (dF peaks) -------------------------------
 ID0025.posXY$Mean.dF <- rowMeans(dFPOS)
 ID0025.posXY$Mean.dF.N <- apply(as.matrix(ID0025.posXY$Mean.dF), 2,
-                                             function(x) (x - min(x))/(max(x)-min(x)))
+                                function(x) (x - min(x))/(max(x)-min(x)))
 
 
 
-# Average activity per cell (deconvolved peaks)
-# then plot on graph
-ID0025.posXY$Mean <- rowMeans(spks)
+# Calculating percentage of active RFP+ cells over time --------------------
+dF.RFP <- subset(spksthresholded, rownames(dFPOS) %in% ID0025.RFPcells) #select only RFP cells
+##ggplot to show percentage of RPF+ cells over time
+RFPsum <- as.data.frame(colSums(RFP))
+RFPsum$Time <- 0:(nrow(RFPsum)-1)
+RFPsum$Perc <- RFPsum$`colSums(RFP)`/nrow(RFP)*100
+ID0025.RFPsum.dF.plt <- ggplot(RFPsum, aes(Time, Perc))+
+  geom_line()+
+  theme_pubr()
 
 
+
+# Isolate RFP+ XY from posXY dataframe ------------------------------------
+ID0025.posXY$RFP <- ID0025.posXY$Cell %in% ID0025.RFPcells
+
+ID0025.posXY.RFP <- ID0025.posXY[which(ID0025.posXY$RFP=="TRUE"),]
 
 
 
@@ -279,15 +303,6 @@ ID0025.posXY$Mean <- rowMeans(spks)
 
 
 
-# Calculating percentage of active RFP cells over time
-dF.RFP <- subset(spksthresholded, rownames(dFPOS) %in% RFPcells) #select only RFP cells
-##ggplot to show percentage of RPF+ cells over time
-RFPsum <- as.data.frame(colSums(RFP))
-RFPsum$Time <- 0:(nrow(RFPsum)-1)
-RFPsum$Perc <- RFPsum$`colSums(RFP)`/nrow(RFP)*100
-ID0025_RFPsum.plt <- ggplot(RFPsum, aes(Time, Perc))+
-  geom_line()+
-  theme_pubr()
 
 
 
@@ -301,10 +316,7 @@ ID0025_RFPsum.plt <- ggplot(RFPsum, aes(Time, Perc))+
 
 
 
-#RFP XY
-ID0025.posXY$RFP <- ID0025.posXY$Cell %in% RFPcells
 
-ID0025.posXY.RFP <- ID0025.posXY[which(ID0025.posXY$RFP=="TRUE"),]
 
 
 # Plot RFP deconvolved curves all together
