2f075cb2042684e41cff47e6f6b2a1965e8542b0
Simo <75310327+simo-91@users.noreply.github.com>
2022-10-28 11:25:16 +0100

    Add files via upload

--- clustering_coefficient.R
+++ clustering_coefficient.R
@@ -12,14 +12,15 @@ CTRL.clustcoeff.RFP <- c(ID0040.clustcoeff.RFP,ID0041.clustcoeff.RFP,
                             ID0044.clustcoeff.RFP,ID0045.clustcoeff.RFP,
                             ID0046.clustcoeff.RFP)
 
-clustcoeff.df <- as.data.frame((cbind(CTRL.clustcoeff.RFP, HRASV12.clustcoeff.RFP)))
-clustcoeff.df <- melt(clustcoeff.df)
-colnames(clustcoeff.df) <- c("Condition", "Clustering Coefficient")
+clustcoeff.df.RFP <- as.data.frame((cbind(CTRL.clustcoeff.RFP, HRASV12.clustcoeff.RFP)))
+clustcoeff.df.RFP <- melt(clustcoeff.df.RFP)
+colnames(clustcoeff.df.RFP) <- c("Condition", "Clustering Coefficient")
+levels(clustcoeff.df.RFP$Condition) <- c("CTRL RFP", "HRASV12 RFP")
 
-
-ggboxplot(clustcoeff.df, x = "Condition", y = "Clustering Coefficient",
+ggboxplot(clustcoeff.df.RFP, x = "Condition", y = "Clustering Coefficient",
           color = "Condition", add = "jitter")+
-  stat_compare_means()
+          # stat_compare_means(label = "p.format", label.y = 1, label.x.npc = "centre", paired = FALSE)+
+          geom_signif(comparisons = list(c("CTRL RFP", "HRASV12 RFP")), map_signif_level = TRUE)
 
 
 # Global efficiency comparison
