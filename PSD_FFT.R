7770cd8abf2ab4d1b4f2ace21c9a755cc8fff4dc
Simo <sim.lenci@gmail.com>
2023-04-11 13:27:57 +0100

    got rid of Mode and changed with arithmetic Mean to calculate overall average PSD

--- PSD_FFT.R
+++ PSD_FFT.R
@@ -1,25 +1,26 @@
-# Mode function
-Mode <- function(x) {
-  ux <- unique(x)
-  ux[which.max(tabulate(match(x, ux)))]
-}
-
-
-# Calculate Fourier transform of each row
+## Calculate Fourier transform of each row
 fft <- apply(spks, 1, fft)
 
 # Calculate power spectrum for each time series
 psd <- abs(fft)^2/ncol(spks) # normalized by length of acquisition
 fs = 0.5 #sampling freq in Hertz; we take a sample every two seconds (1 sample/2 seconds = 0.5)
 nyquist <- fs/2
-freq <- seq(0, nyquist, length.out=nrow(psd)) # calculate frequency range
 
+psd <- round(psd, digits = 1)
+freq <- seq(0, nyquist, length.out=nrow(psd)) # calculate frequency range
+psd.melt <- as.data.frame(psd)
 psd.melt$frequency <- freq
-psd.melt <- melt(psd, id.vars = "frequency", variable.name = "cell", value.name = "PSD")
-psd.mean <- summarise(psd.melt, ave = Mode(PSD), .by = "frequency")
+psd.melt <- melt(psd.melt, id.vars = "frequency", variable.name = "cell", value.name = "PSD")
+
+ID0147.psd.mean <- summarise(psd.melt, ID0147.mean = mean(PSD), .by = "frequency")
+
+
 # Plot
-ggline(psd.mean, x = "frequency", y = "ave",
-       plot_type = "l")
+ggplot(ID0147.psd.mean, aes(x = frequency, y = `ID0147.mean`))+
+  geom_line()
+
+
+# ggpar(plt, ticks = TRUE, xticks.by = 1)
 
 
 # plot(freq, psd[,1], type = "l", main = "Power Density Spectrum", xlab = "Hz", ylab = "Power")
@@ -29,6 +30,3 @@ ggline(psd.mean, x = "frequency", y = "ave",
 #             }
 # psd.plt <- recordPlot()
 
-# Using Mode as average to count frequency of frequencies in the whole population?
-tab <- table(psd)
-freq_mode <-as.numeric(names(tab)[which.max(tab)])
