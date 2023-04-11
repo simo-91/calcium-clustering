c98e4e77570d5043348720a75b4ea1bdb9d00040
Simo <sim.lenci@gmail.com>
2023-04-11 13:39:23 +0100

    implemented RFP only PSD analysis

--- PSD_FFT.R
+++ PSD_FFT.R
@@ -1,4 +1,4 @@
-## Calculate Fourier transform of each row
+```{r Calculate Fourier transform of each row}
 fft <- apply(spks, 1, fft)
 
 # Calculate power spectrum for each time series
@@ -11,6 +11,7 @@ freq <- seq(0, nyquist, length.out=nrow(psd)) # calculate frequency range
 psd.melt <- as.data.frame(psd)
 psd.melt$frequency <- freq
 psd.melt <- melt(psd.melt, id.vars = "frequency", variable.name = "cell", value.name = "PSD")
+```
 
 ID0147.psd.mean <- summarise(psd.melt, ID0147.mean = mean(PSD), .by = "frequency")
 
@@ -20,13 +21,19 @@ ggplot(ID0147.psd.mean, aes(x = frequency, y = `ID0147.mean`))+
   geom_line()
 
 
-# ggpar(plt, ticks = TRUE, xticks.by = 1)
 
+```{r for RFP cells only}
+fft.RFP <- apply(RFP, 1, fft)
 
-# plot(freq, psd[,1], type = "l", main = "Power Density Spectrum", xlab = "Hz", ylab = "Power")
-#             # loop through each column and add a line to the plot
-#             for (i in 2:ncol(psd)) {
-#               lines(freq, psd[,i], col = "grey")
-#             }
-# psd.plt <- recordPlot()
+# Calculate power spectrum for each time series
+psd.RFP <- abs(fft.RFP)^2/ncol(RFP) # normalized by length of acquisition
+fs = 0.5 #sampling freq in Hertz; we take a sample every two seconds (1 sample/2 seconds = 0.5)
+nyquist <- fs/2
+
+psd.RFP <- round(psd.RFP, digits = 1)
+freq <- seq(0, nyquist, length.out=nrow(psd.RFP)) # calculate frequency range
+psd.RFP.melt <- as.data.frame(psd.RFP)
+psd.RFP.melt$frequency <- freq
+psd.RFP.melt <- melt(psd.RFP.melt, id.vars = "frequency", variable.name = "cell", value.name = "PSD")
+```
 
